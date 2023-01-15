d7b6b4509a4b96ab4b13de530fa0186a8ea13ee1
Sabotage971 <sabotage971@gmail.com>
2023-01-15 23:19:27 +0500

    добавил сервис

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import web.dao.CarDao;
-import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +14,7 @@ import java.util.List;
 public class HelloController {
 
 	@Autowired
-	private CarService service;
+	private CarServiceImpl service;
 
 	@GetMapping(value = "/")
 	public String printWelcome(ModelMap model) {
